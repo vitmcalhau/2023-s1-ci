c7f94f6f73c89c499f03cf353e20a6be3fd7ce01
vitmcalhau <v248740@dac.unicamp.br>
2023-04-13 19:55:38 -0300

    adiciona teste caracteres especiais nao permitidos

--- simple_web_app/test_unit.py
+++ simple_web_app/test_unit.py
@@ -67,3 +67,10 @@ def test_8_chars_with_no_upper_case_password_should_raise_exception():
     with pytest.raises(Exception):  # then
         # when
         validator.password_validate()
+
+def test_8_chars_with_not_allowed_especial_chars_password_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="/Ab1^c2~")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
